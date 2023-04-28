8fb6bad205e6655ac1cb963113b9e4f085bc68ae
S1mple133 <pavelescu_darius@yahoo.com>
2023-04-29 00:37:20 +0200

    fix: fix spectator mode on player death in hardcore server

--- src/main/java/org/matrixnetwork/matrixlifesystem/listener/LoseLifeDeathListener.java
+++ src/main/java/org/matrixnetwork/matrixlifesystem/listener/LoseLifeDeathListener.java
@@ -4,8 +4,14 @@ import co.aikar.locales.MessageKey;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentBuilder;
+import net.kyori.adventure.text.format.TextColor;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.matrixnetwork.matrixlifesystem.Constants;
 import org.matrixnetwork.matrixlifesystem.MatrixLifeSystem;
@@ -16,20 +22,24 @@ public class LoseLifeDeathListener implements Listener {
     private final MatrixLifeSystem plugin = MatrixLifeSystem.instance();
 
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        if(event.getPlayer().hasPermission(Constants.ACF_ADMIN_PERMISSION)) {
-            return;
-        }
+    public void onPlayerDeath(EntityDamageByEntityEvent event) {
+        if(event.getEntity() instanceof Player player) {
+            if(player.getHealth() - event.getDamage() >= 1) {
+                return;
+            }
+            String uuid = player.getUniqueId().toString();
+            PlayerData pd = PlayerData.getPlayerData(uuid);
 
-        PlayerData.removeLife(PlayerData.getPlayerData(event.getPlayer().getUniqueId().toString()));
+            PlayerData.removeLife(PlayerData.getPlayerData(uuid));
 
-        if(PlayerData.getPlayerData(event.getPlayer().getUniqueId().toString()).getLives() < plugin.getMinLifes()) {
-            MLSUtil.movePlayer(event.getPlayer());
-           /* event.getPlayer()
-                    .kick(Component.text(plugin.getCommandManager()
-                            .getLocales()
-                            .getMessage(null, MessageKey.of("kick.message")
-                            )));*/
+            event.setCancelled(true);
+            player.setHealth(player.getMaxHealth());
+            player.teleport(player.getWorld().getSpawnLocation());
+            player.sendMessage(ChatColor.RED + "You died! Lives left: " + pd.getLives());
+
+            if(pd.getLives() < plugin.getMinLifes() && !player.hasPermission(Constants.ACF_ADMIN_PERMISSION)) {
+                MLSUtil.movePlayer(player);
+            }
         }
     }
 }
