21de444a1908de26503fd56650cc72ad2106afe4
S1mple133 <pavelescu_darius@yahoo.com>
2023-04-28 16:31:40 +0200

    fix: teleport the player to the classic server instead of kicking

--- src/main/java/org/matrixnetwork/matrixlifesystem/listener/LoseLifeDeathListener.java
+++ src/main/java/org/matrixnetwork/matrixlifesystem/listener/LoseLifeDeathListener.java
@@ -1,6 +1,8 @@
 package org.matrixnetwork.matrixlifesystem.listener;
 
 import co.aikar.locales.MessageKey;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,6 +10,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.matrixnetwork.matrixlifesystem.Constants;
 import org.matrixnetwork.matrixlifesystem.MatrixLifeSystem;
 import org.matrixnetwork.matrixlifesystem.entity.PlayerData;
+import org.matrixnetwork.matrixlifesystem.util.MLSUtil;
 
 public class LoseLifeDeathListener implements Listener {
     private final MatrixLifeSystem plugin = MatrixLifeSystem.instance();
@@ -21,11 +24,12 @@ public class LoseLifeDeathListener implements Listener {
         PlayerData.removeLife(PlayerData.getPlayerData(event.getPlayer().getUniqueId().toString()));
 
         if(PlayerData.getPlayerData(event.getPlayer().getUniqueId().toString()).getLives() < plugin.getMinLifes()) {
-            event.getPlayer()
+            MLSUtil.movePlayer(event.getPlayer());
+           /* event.getPlayer()
                     .kick(Component.text(plugin.getCommandManager()
                             .getLocales()
                             .getMessage(null, MessageKey.of("kick.message")
-                            )));
+                            )));*/
         }
     }
 }
---
+++ src/main/java/org/matrixnetwork/matrixlifesystem/util/MLSUtil.java
@@ -0,0 +1,30 @@
+package org.matrixnetwork.matrixlifesystem.util;
+
+import co.aikar.locales.MessageKey;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.matrixnetwork.matrixlifesystem.MatrixLifeSystem;
+
+public class MLSUtil {
+    private static MatrixLifeSystem plugin = MatrixLifeSystem.instance();
+    private static String fallbackServer = MatrixLifeSystem.instance().getFallbackServer();
+
+    public static void movePlayer(Player player) {
+        if(!plugin.getServer().getName().equalsIgnoreCase(fallbackServer)) {
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Message");
+            out.writeUTF(Component.text(plugin.getCommandManager()
+                    .getLocales()
+                    .getMessage(null, MessageKey.of("kick.message")
+                    )).toString());
+            player.sendPluginMessage(MatrixLifeSystem.instance(), "BungeeCord", out.toByteArray());
+
+            out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF(MatrixLifeSystem.instance().getFallbackServer());
+            player.sendPluginMessage(MatrixLifeSystem.instance(), "BungeeCord", out.toByteArray());
+        }
+    }
+}
