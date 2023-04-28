04b5e20004919ae696578dd068ad822f58d34495
S1mple133 <pavelescu_darius@yahoo.com>
2023-04-29 00:54:45 +0200

    fix: respawn instead of canceling death event

--- src/main/java/org/matrixnetwork/matrixlifesystem/listener/LoseLifeDeathListener.java
+++ src/main/java/org/matrixnetwork/matrixlifesystem/listener/LoseLifeDeathListener.java
@@ -4,14 +4,8 @@ import co.aikar.locales.MessageKey;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.ComponentBuilder;
-import net.kyori.adventure.text.format.TextColor;
-import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.matrixnetwork.matrixlifesystem.Constants;
 import org.matrixnetwork.matrixlifesystem.MatrixLifeSystem;
@@ -22,24 +16,16 @@ public class LoseLifeDeathListener implements Listener {
     private final MatrixLifeSystem plugin = MatrixLifeSystem.instance();
 
     @EventHandler
-    public void onPlayerDeath(EntityDamageByEntityEvent event) {
-        if(event.getEntity() instanceof Player player) {
-            if(player.getHealth() - event.getDamage() >= 1) {
-                return;
-            }
-            String uuid = player.getUniqueId().toString();
-            PlayerData pd = PlayerData.getPlayerData(uuid);
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        event.getPlayer().spigot().respawn();
+        if(event.getPlayer().hasPermission(Constants.ACF_ADMIN_PERMISSION)) {
+            return;
+        }
 
-            PlayerData.removeLife(PlayerData.getPlayerData(uuid));
+        PlayerData.removeLife(PlayerData.getPlayerData(event.getPlayer().getUniqueId().toString()));
 
-            event.setCancelled(true);
-            player.setHealth(player.getMaxHealth());
-            player.teleport(player.getWorld().getSpawnLocation());
-            player.sendMessage(ChatColor.RED + "You died! Lives left: " + pd.getLives());
-
-            if(pd.getLives() < plugin.getMinLifes() && !player.hasPermission(Constants.ACF_ADMIN_PERMISSION)) {
-                MLSUtil.movePlayer(player);
-            }
+        if(PlayerData.getPlayerData(event.getPlayer().getUniqueId().toString()).getLives() < plugin.getMinLifes()) {
+            MLSUtil.movePlayer(event.getPlayer());
         }
     }
 }
